c6e3171ecb2bb058e5c673277fd0c118018273b7
awk1912 <awk19121993@gmail.com>
2019-02-07 13:26:15 +0700

    test pull

--- final challenge/RootTabBar/RootTabBarViewController.swift
+++ final challenge/RootTabBar/RootTabBarViewController.swift
@@ -13,12 +13,6 @@ class RootTabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        
-        
-        self.tabBarController?.toolbarItems?[0].image = #imageLiteral(resourceName: "record")
-        self.tabBarItem.image = #imageLiteral(resourceName: "stop")
-        
     }
     
 
