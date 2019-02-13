bbfd8d3b2418d6e0d478218b5823618191469b4f
adisr21 <sukarnorachman@gmail.com>
2019-02-13 21:13:40 +0700

    segue core data history to hasil

--- final challenge/Hasil/HasilViewController.swift
+++ final challenge/Hasil/HasilViewController.swift
@@ -7,13 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
+import CoreData
 
-class HasilViewController: UIViewController {
+class HasilViewController: UIViewController, NSFetchedResultsControllerDelegate {
+    
+    
     @IBOutlet weak var viewHasil: UIView!
     @IBOutlet weak var viewRecording: UIView!
     @IBOutlet weak var kontenHasil: UITextView!
     
-    var valueWpm = 125
+    var selectedIndexPath: IndexPath!
+    var audioData: Data!
+    var valueWpm: Float!
+    var durasiAudio: Float!
+    
+    fileprivate func setupReview() {
+        if valueWpm >= 200 { kontenHasil.text = "Rata-rata kecepatan bicaramu adalah \(valueWpm) kata per menit.\n\nKecepatan bicara ini tergolong terlalu cepat untuk presentasi pada umumnya, jika tidak diperlambat, pendengar akan merasa sangat kesulitan mengikuti alur pembicaraanmu. Kecepatan ini pada umumnya hanya digunakan oleh juru lelang yang ingin menciptakan rasa mendesak pada para pendengarnya."} else
+            if valueWpm >= 150 { kontenHasil.text = "Rata-rata kecepatan bicaramu adalah \(valueWpm) kata per menit.\n\nKecepatan bicara ini tergolong cepat untuk presentasi pada umumnya, jika tidak diperlambat, pendengar akan merasa kesulitan mengikuti alur pembicaraanmu. Kecepatan ini sebaiknya digunakan untuk menyampaikan poin-poin yang bersemangat atau antusiasme yang kuat."} else
+                if valueWpm >= 120 { kontenHasil.text = "Rata-rata kecepatan bicaramu adalah \(valueWpm) kata per menit.\n\nKecepatan bicara anda sangat baik untuk presentasi pada umumnya, kecepatan ini membuat pendengar merasa sedang diajak dalam percakapan sehari-hari."} else
+                    if valueWpm < 120 { kontenHasil.text = "Rata-rata kecepatan bicaramu adalah \(valueWpm) kata per menit.\n\nKecepatan bicara ini tergolong lambat untuk presentasi pada umumnya, jika tidak dipercepat, pendengar dapat merasa bosan bahkan mengantuk. Kecepatan ini sebaiknya digunakan untuk menyampaikan poin-poin krusial yang perlu dipahami dengan dalam oleh pendengar."} else
+                    {kontenHasil.text = "Unidentified."}
+    }
+    
+    var fetchedResultsController: NSFetchedResultsController<Audio>!
+    
+    let coreDataAudio = CoreDataAudio(modelName: "final_challenge")
+    
+    func initializeFetchedResultsController(){
+        let request = NSFetchRequest<Audio>(entityName: "Audio")
+        let dateSort = NSSortDescriptor(key: "date", ascending: false)
+        
+        request.sortDescriptors = [dateSort]
+        
+        let moc = coreDataAudio.managedObjectContext
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: moc, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultsController.delegate = self
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("Failed to initialize FetchedResultsController: \(error)")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,17 +63,25 @@ class HasilViewController: UIViewController {
         viewRecording.layer.borderColor = UIColor.orange.cgColor
         viewRecording.clipsToBounds = true
         viewRecording.layer.cornerRadius = 10.0
-        
-        if valueWpm >= 200 { kontenHasil.text = "Rata-rata kecepatan bicaramu adalah \(valueWpm) kata per menit.\n\nKecepatan bicara ini tergolong terlalu cepat untuk presentasi pada umumnya, jika tidak diperlambat, pendengar akan merasa sangat kesulitan mengikuti alur pembicaraanmu. Kecepatan ini pada umumnya hanya digunakan oleh juru lelang yang ingin menciptakan rasa mendesak pada para pendengarnya."} else
-            if valueWpm >= 150 { kontenHasil.text = "Rata-rata kecepatan bicaramu adalah \(valueWpm) kata per menit.\n\nKecepatan bicara ini tergolong cepat untuk presentasi pada umumnya, jika tidak diperlambat, pendengar akan merasa kesulitan mengikuti alur pembicaraanmu. Kecepatan ini sebaiknya digunakan untuk menyampaikan poin-poin yang bersemangat atau antusiasme yang kuat."} else
-                if valueWpm >= 120 { kontenHasil.text = "Rata-rata kecepatan bicaramu adalah \(valueWpm) kata per menit.\n\nKecepatan bicara anda sangat baik untuk presentasi pada umumnya, kecepatan ini membuat pendengar merasa sedang diajak dalam percakapan sehari-hari."} else
-                    if valueWpm < 120 { kontenHasil.text = "Rata-rata kecepatan bicaramu adalah \(valueWpm) kata per menit.\n\nKecepatan bicara ini tergolong lambat untuk presentasi pada umumnya, jika tidak dipercepat, pendengar dapat merasa bosan bahkan mengantuk. Kecepatan ini sebaiknya digunakan untuk menyampaikan poin-poin krusial yang perlu dipahami dengan dalam oleh pendengar."} else
-                    {kontenHasil.text = "Unidentified."}
+        kontenHasil.isEditable = false
         
         // Do any additional setup after loading the view.
     }
     
 
+    override func viewWillAppear(_ animated: Bool) {
+        self.initializeFetchedResultsController()
+        self.setupData()
+        self.setupReview()
+    }
+    
+    func setupData(){
+        print("indexPath : \(self.selectedIndexPath)")
+        let audio = fetchedResultsController.object(at: self.selectedIndexPath)
+        
+        valueWpm = audio.wpm
+        print("nilai wpm: \(valueWpm)")
+    }
     /*
     // MARK: - Navigation
 
